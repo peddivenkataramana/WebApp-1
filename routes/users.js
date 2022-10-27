513c6c46f81e9f8cfa7f4ba31d81e9a112413097
peddivenkataramana <peddivenkataramana78@gmail.com>
2022-10-26 22:51:25 -0500

    final

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+times=0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  times+=1;
+  res.send('users accesses : '+times);
 });
 
 module.exports = router;
